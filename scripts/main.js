51a8fbcca858f417ac930bddd28ac70b25cc04a7
aashish-arun <aashish.ouo@gmail.com>
2025-02-04 23:13:07 -0700

    Day-7

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+function expandWho() {
+    document.getElementById("stylewho").innerHTML = "Yo, I dont like being bored, so I keep myself occupied with hobbies like gaming, watching anime, and reading manga, manhua, and manhwa. I also try to learn new things, most of which revolve around computers and programming as to not burned out of my other hobbies.";
+}
